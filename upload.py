ef274571be5bd0f6090abfd9fbea2030eacc166a
Daniel Liu <xyfc128@gmail.com>
2025-05-30 13:37:30 +0800

    fix: also upload tokenizer

--- upload.py
+++ upload.py
@@ -1,9 +1,12 @@
-from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 if __name__ == "__main__":
     model_name = "./llama3.2-1b-distilled"
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
     )
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    
     model.name = "llama3.2-1b-distilled"
     model.push_to_hub("btliu/llama3.2-1b-distilled")
+    tokenizer.push_to_hub("btliu/llama3.2-1b-distilled")
